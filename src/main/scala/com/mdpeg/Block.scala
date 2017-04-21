a5eb4ad4776694c064424467a92030b3d89fbb3c
DRouh <dimerouh@gmail.com>
2017-04-21 09:50:53 +0300

    Add block level AST cases

---
+++ src/main/scala/com/mdpeg/Block.scala
@@ -0,0 +1,12 @@
+package com.mdpeg
+
+sealed trait Block
+
+final case class BlockQuote(inline: String) extends Block
+final case class Verbatim(inline: String) extends Block
+final case class HeadingBlock(inline: String) extends Block
+final case class ListBlock(inline: String) extends Block
+final case class HorizontalRuleBlock(inline: String) extends Block
+final case class Para(inline: String) extends Block
+final case class Paragraph(inline: String) extends Block
+final case class TableBlock(inline: String) extends Block
