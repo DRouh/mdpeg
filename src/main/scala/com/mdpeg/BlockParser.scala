7eabab34ce61f54deafe1692e3af69a1f3ab4159
DRouh <dimerouh@gmail.com>
2017-04-22 09:00:34 +0300

    Improve paragraph recognition

--- src/main/scala/com/mdpeg/BlockParser.scala
+++ src/main/scala/com/mdpeg/BlockParser.scala
@@ -4,18 +4,18 @@ import org.parboiled2._
 
 class BlockParser(val input: ParserInput) extends Parser {
   import CharPredicate._
-  def InputLine = rule(block ~ EOI)
+  def InputLine = rule(block.+ ~ EOI)
 
-  def block = rule { para | plain }
+  def block = rule { plain }
 
   //block definitions
-  def para  = rule { inline.+ ~ nl ~ blankLine.+ }
-  def plain = rule { inline.+ ~ blankLine.? }
+  //def para  = rule { inline.+ ~ nl ~ blankLine.+ }
+  def plain = rule { capture(inline.+) ~ blankLine.? ~> Paragraph}
 
   //aux functions
   //def inline          = rule { AlphaNum | sp | nl | punctuationChar | anyOf("_\"{}()") }
   def inline          = rule { AlphaNum | sp | punctuationChar | anyOf("_\"{}()") }
-  def blankLine       = rule { capture(sp ~ nl) }
+  def blankLine       = rule { sp ~ nl }
   def punctuationChar = rule { anyOf(":;,.?!-") }
   def nl              = rule { "\r\n" | "\r" | "\n" }
   def sp              = rule { " " | "\t" }
