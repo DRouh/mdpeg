30c723fd697a436bf72368c112e26dcdefcd5969
DRouh <dimerouh@gmail.com>
2017-04-22 19:57:49 +0300

    Rearrange case classes

--- src/main/scala/com/mdpeg/Block.scala
+++ src/main/scala/com/mdpeg/Block.scala
@@ -2,11 +2,11 @@ package com.mdpeg
 
 sealed trait Block
 
-final case class BlockQuote(inline: String) extends Block
-final case class Verbatim(inline: String) extends Block
-final case class HeadingBlock(level: Int, inline: String) extends Block
-final case class ListBlock(inline: String) extends Block
-final case class HorizontalRuleBlock(inline: String) extends Block
-final case class Paragraph(inline: String) extends Block
 final case class Plain(inline: String) extends Block
+final case class Paragraph(inline: String) extends Block
+final case class HorizontalRuleBlock(inline: String) extends Block
+final case class ListBlock(inline: String) extends Block
+final case class HeadingBlock(level: Int, inline: String) extends Block
+final case class Verbatim(inline: String) extends Block
+final case class BlockQuote(inline: String) extends Block
 final case class TableBlock(inline: String) extends Block
