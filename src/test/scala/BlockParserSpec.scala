40b651c4bcfd09f8e6e999a10a9c3f3cc78d7495
DRouh <dimerouh@gmail.com>
2017-04-23 14:58:14 +0300

    Add positive tests for Block Quote, Paragraph, Plain text

--- src/test/scala/BlockParserSpec.scala
+++ src/test/scala/BlockParserSpec.scala
@@ -3,7 +3,7 @@ package com.mdpeg
 import org.scalatest.{FlatSpec, Matchers}
 
 class BlockParserSpec extends FlatSpec with Matchers {
-  it should "parse a '_' Horizontal rule" in {
+  it should "parse '_' Horizontal rule" in {
     val term =
       """   ____
         |
@@ -12,7 +12,7 @@ class BlockParserSpec extends FlatSpec with Matchers {
     parsed shouldEqual HorizontalRuleBlock
   }
 
-  it should "parse a '-' sparse Horizontal rule" in {
+  it should "parse '-' sparse Horizontal rule" in {
     val term =
       """   - - - -
         |
@@ -21,12 +21,44 @@ class BlockParserSpec extends FlatSpec with Matchers {
     parsed shouldEqual HorizontalRuleBlock
   }
 
-  it should "parse a '*'-many Horizontal rule" in {
+  it should "parse '*'-many Horizontal rule" in {
     val term =
       """   ***************
         |
         |""".stripMargin
     val parsed = new BlockParser(term).horizontalRule.run().get
     parsed shouldEqual HorizontalRuleBlock
+  }
+
+  it should "parse '>' Block Quote" in {
+    val term =
+      """> Hello this is block quote
+        |> this is continuation of a block quote
+        |> yet another line for the block""".stripMargin
+
+    val expectedText =
+      """Hello this is block quote
+        |this is continuation of a block quote
+        |yet another line for the block""".stripMargin
+    val parsed = new BlockParser(term).blockQuote.run().get
+    parsed shouldEqual BlockQuote(expectedText)
+  }
+
+  it should "parse Paragraph" in {
+    val term =
+      """It is a long established fact that a reader will be distracted by the
+        |
+        |
+        """.stripMargin
+    val expected = "It is a long established fact that a reader will be distracted by the"
+    val parsed = new BlockParser(term).paragraph.run().get
+    parsed shouldEqual Paragraph(expected)
+  }
+
+  it should "parse Plain text" in {
+    val term = "It is a long established fact that a reader will be distracted by the"
+    val expected = "It is a long established fact that a reader will be distracted by the"
+    val parsed = new BlockParser(term).plain.run().get
+    parsed shouldEqual Plain(expected)
   }
 }
