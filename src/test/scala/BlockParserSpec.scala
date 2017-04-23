b52aab89faa21fd85142603e632927f1dbcf9598
DRouh <dimerouh@gmail.com>
2017-04-23 18:21:30 +0300

    Add test for compound markdown document

--- src/test/scala/BlockParserSpec.scala
+++ src/test/scala/BlockParserSpec.scala
@@ -73,4 +73,42 @@ class BlockParserSpec extends FlatSpec with Matchers {
       parsed shouldEqual HeadingBlock(t._1, expected)
     }
   }
+
+  it should "parse a compound document" in {
+    val term = TestData.compoundMD
+    val parsed = new BlockParser(term).InputLine.run().get
+    val headingOne = "Heading One"
+    val headingTwo = "Heading Two"
+    val paragraphOne ="""It is a long established fact that a reader will be distracted by the readable content
+        |of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+        |more-or-less normal distribution of letters, as opposed to using, 'Content content'
+        |making it look like readable English. Many desktop publishing packages and web page editors
+        |now use Lorem Ipsum as their default model text, and a search for will uncover
+        |many web sites still in their infancy. Various versions have evolved over the years,
+        |sometimes by accident, sometimes on purpose (injected humour and the like).""".stripMargin
+    val paragraphTwo = """But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
+        |and I will give you a complete account of the system, and expound the actual teachings of the great
+        |explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids
+        |pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure
+        |rationally encounter consequences that are extremely painful. Nor again is there anyone who loves
+        |or pursues or desires to obtain pain of itself, because it is pain, but because occasionally
+        |circumstances occur in which toil and pain can procure him some great pleasure.
+        |To take a trivial example, which of""".stripMargin
+    val blockQuote ="""This is quote
+        |and should span several
+        |lines
+        |""".stripMargin
+
+    val plainText = "This is a plaint string in the end."
+    parsed shouldEqual Vector(
+      HeadingBlock(1, headingOne),
+      HeadingBlock(2, headingTwo),
+      Paragraph(paragraphOne),
+      Paragraph(paragraphTwo),
+      HorizontalRuleBlock,
+      HorizontalRuleBlock,
+      BlockQuote(blockQuote),
+      HorizontalRuleBlock,
+      Plain(plainText))
+  }
 }
