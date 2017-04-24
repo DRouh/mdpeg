0f87dcd8ae75ef063d76fe68905ca992cba6816c
DRouh <dimerouh@gmail.com>
2017-04-24 09:24:14 +0300

    Refactor tests

--- src/test/scala/BlockParserSpec.scala
+++ src/test/scala/BlockParserSpec.scala
@@ -31,84 +31,56 @@ class BlockParserSpec extends FlatSpec with Matchers {
 
   it should "parse '>' Block Quote" in {
     val term =
-      """> Hello this is block quote
-        |> this is continuation of a block quote
-        |> yet another line for the block""".stripMargin
+      s"""> ${TestData.blockQuoteLineOne}
+        |> ${TestData.blockQuoteLineTwo}
+        |> ${TestData.blockQuoteLineThree}
+        |""".stripMargin
 
-    val expectedText =
-      """Hello this is block quote
-        |this is continuation of a block quote
-        |yet another line for the block""".stripMargin
+    val expectedText = TestData.blockQuote
     val parsed = new BlockParser(term).blockQuote.run().get
     parsed shouldEqual BlockQuote(expectedText)
   }
 
   it should "parse Paragraph" in {
     val term =
-      """It is a long established fact that a reader will be distracted by the
+      s"""${TestData.paragraphOne}
         |
         |
         """.stripMargin
-    val expected = "It is a long established fact that a reader will be distracted by the"
     val parsed = new BlockParser(term).paragraph.run().get
-    parsed shouldEqual Paragraph(expected)
+    parsed shouldEqual Paragraph(TestData.paragraphOne)
   }
 
   it should "parse Plain text" in {
-    val term = "It is a long established fact that a reader will be distracted by the"
-    val expected = "It is a long established fact that a reader will be distracted by the"
+    val term = TestData.plainText
     val parsed = new BlockParser(term).plain.run().get
-    parsed shouldEqual Plain(expected)
+    parsed shouldEqual Plain(TestData.plainText)
   }
 
   it should "parse ATX Heading" in {
     val term =
-      """ Test your header
+      s""" ${TestData.headingOne}
         |
       """.stripMargin
-    val expected = "Test your header"
     val ts = (1 to 6).map(i => (i, "#" * i + term))
     for (t <- ts) {
       val parsed = new BlockParser(t._2).heading.run().get
-      parsed shouldEqual HeadingBlock(t._1, expected)
+      parsed shouldEqual HeadingBlock(t._1, TestData.headingOne)
     }
   }
 
   it should "parse a compound document" in {
     val term = TestData.compoundMD
     val parsed = new BlockParser(term).InputLine.run().get
-    val headingOne = "Heading One"
-    val headingTwo = "Heading Two"
-    val paragraphOne ="""It is a long established fact that a reader will be distracted by the readable content
-        |of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-        |more-or-less normal distribution of letters, as opposed to using, 'Content content'
-        |making it look like readable English. Many desktop publishing packages and web page editors
-        |now use Lorem Ipsum as their default model text, and a search for will uncover
-        |many web sites still in their infancy. Various versions have evolved over the years,
-        |sometimes by accident, sometimes on purpose (injected humour and the like).""".stripMargin
-    val paragraphTwo = """But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
-        |and I will give you a complete account of the system, and expound the actual teachings of the great
-        |explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids
-        |pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure
-        |rationally encounter consequences that are extremely painful. Nor again is there anyone who loves
-        |or pursues or desires to obtain pain of itself, because it is pain, but because occasionally
-        |circumstances occur in which toil and pain can procure him some great pleasure.
-        |To take a trivial example, which of""".stripMargin
-    val blockQuote ="""This is quote
-        |and should span several
-        |lines
-        |""".stripMargin
-
-    val plainText = "This is a plaint string in the end."
     parsed shouldEqual Vector(
-      HeadingBlock(1, headingOne),
-      HeadingBlock(2, headingTwo),
-      Paragraph(paragraphOne),
-      Paragraph(paragraphTwo),
+      HeadingBlock(1, TestData.headingOne),
+      HeadingBlock(2, TestData.headingTwo),
+      Paragraph(TestData.paragraphOne),
+      Paragraph(TestData.paragraphTwo),
       HorizontalRuleBlock,
       HorizontalRuleBlock,
-      BlockQuote(blockQuote),
+      BlockQuote(TestData.blockQuote),
       HorizontalRuleBlock,
-      Plain(plainText))
+      Plain(TestData.plainText))
   }
 }
--- src/test/scala/TestData.scala
+++ src/test/scala/TestData.scala
@@ -1,34 +1,53 @@
 object TestData {
-  val compoundMD = """# Heading One
-                     |
-                     |## Heading Two
-                     |
-                     |It is a long established fact that a reader will be distracted by the readable content
-                     |of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-                     |more-or-less normal distribution of letters, as opposed to using, 'Content content'
-                     |making it look like readable English. Many desktop publishing packages and web page editors
-                     |now use Lorem Ipsum as their default model text, and a search for will uncover
-                     |many web sites still in their infancy. Various versions have evolved over the years,
-                     |sometimes by accident, sometimes on purpose (injected humour and the like).
-                     |
-                     |But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
-                     |and I will give you a complete account of the system, and expound the actual teachings of the great
-                     |explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids
-                     |pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure
-                     |rationally encounter consequences that are extremely painful. Nor again is there anyone who loves
-                     |or pursues or desires to obtain pain of itself, because it is pain, but because occasionally
-                     |circumstances occur in which toil and pain can procure him some great pleasure.
-                     |To take a trivial example, which of
-                     |
-                     |****
-                     |
-                     |-----
-                     |
-                     |> This is quote
-                     |> and should span several
-                     |> lines
-                     |
-                     |_ _ _ _
-                     |
-                     |This is a plaint string in the end.""".stripMargin
+
+  val headingOne = "Heading One"
+  val headingTwo = "Heading Two"
+  val paragraphOne =
+    """It is a long established fact that a reader will be distracted by the readable content
+    |of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+    |more-or-less normal distribution of letters, as opposed to using, 'Content content'
+    |making it look like readable English. Many desktop publishing packages and web page editors
+    |now use Lorem Ipsum as their default model text, and a search for will uncover
+    |many web sites still in their infancy. Various versions have evolved over the years,
+    |sometimes by accident, sometimes on purpose (injected humour and the like).""".stripMargin
+  val paragraphTwo =
+    """But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
+    |and I will give you a complete account of the system, and expound the actual teachings of the great
+    |explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids
+    |pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure
+    |rationally encounter consequences that are extremely painful. Nor again is there anyone who loves
+    |or pursues or desires to obtain pain of itself, because it is pain, but because occasionally
+    |circumstances occur in which toil and pain can procure him some great pleasure.
+    |To take a trivial example, which of""".stripMargin
+
+  val blockQuoteLineOne = "This is quote"
+  val blockQuoteLineTwo = "and should span several"
+  val blockQuoteLineThree = "yet another line for the block"
+  val blockQuote =
+    s"""${blockQuoteLineOne}
+    |${blockQuoteLineTwo}
+    |${blockQuoteLineThree}
+    |""".stripMargin
+  val plainText = "This is a plaint string in the end."
+  val compoundMD =
+    s"""# ${headingOne}
+       |
+       |## ${headingTwo}
+       |
+       |${paragraphOne}
+       |
+       |${paragraphTwo}
+       |
+       |****
+       |
+       |-----
+       |
+       |> ${blockQuoteLineOne}
+       |> ${blockQuoteLineTwo}
+       |> ${blockQuoteLineThree}
+       |
+       |_ _ _ _
+       |
+       |${plainText}"""
+      .stripMargin
 }
