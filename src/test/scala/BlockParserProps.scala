33e5e1fdff39d0007be6cb61d417477d1dec67f9
DRouh <dimerouh@gmail.com>
2017-04-24 10:35:56 +0300

    Add a property test for Paragraph rule

--- src/test/scala/BlockParserProps.scala
+++ src/test/scala/BlockParserProps.scala
@@ -1,5 +1,17 @@
-import org.scalacheck.Properties
-import org.scalacheck.Prop.forAll
+import com.mdpeg.{BlockParser, Paragraph}
+import org.scalacheck.Prop.{BooleanOperators, forAll}
+import org.scalacheck.{Gen, Properties}
 
-object BlockParserProps extends Properties("String"){
+object BlockParserProps extends Properties("String") {
+  val inline = Gen.alphaNumStr
+  property("Any alpha sequence followed by 'cr or crlf' is a paragraph of that sequence") = forAll(inline) { i =>
+    (i != null && i != "") ==> {
+    val term =
+      s"""${i}
+        |
+        |""".stripMargin
+    val parsed = new BlockParser(term).paragraph.run().get
+    parsed == Paragraph(i)
+    }
+  }
 }
