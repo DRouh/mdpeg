272036f3d1e66c43d57e8f0bdbea182ad471f77c
DRouh <dimerouh@gmail.com>
2017-04-26 10:06:36 +0300

    Initial work on List rule

--- src/main/scala/com/mdpeg/Block.scala
+++ src/main/scala/com/mdpeg/Block.scala
@@ -5,8 +5,18 @@ sealed trait Block
 final case class Plain(inline: String) extends Block
 final case class Paragraph(inline: String) extends Block
 case object HorizontalRuleBlock extends Block
-final case class ListBlock(inline: String) extends Block
 final case class HeadingBlock(level: Int, inline: String) extends Block
 final case class Verbatim(inline: String) extends Block
 final case class BlockQuote(inline: String) extends Block
 final case class TableBlock(inline: String) extends Block
+
+// list cases
+final case class OrderedListBlock(inline: Vector[Block]) extends Block
+final case class UnorderedListBlock(inline: Vector[Block]) extends Block
+
+
+/**
+  * Raw markdown that is yet to be processed into blocks
+  * @param inline - raw string
+  */
+final case class Markdown(inline: String) extends Block
