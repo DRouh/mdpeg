9c86d9397aaecba96cefe4ed2f8097c6c55d6fb2
DRouh <dimerouh@gmail.com>
2017-04-26 09:41:48 +0300

    Fix property object name

--- src/test/scala/BlockParserProps.scala
+++ src/test/scala/BlockParserProps.scala
@@ -2,7 +2,7 @@ import com.mdpeg.{BlockParser, Paragraph}
 import org.scalacheck.Prop.{BooleanOperators, forAll}
 import org.scalacheck.{Gen, Properties}
 
-object BlockParserProps extends Properties("String") {
+object BlockParserProps extends Properties("BlockParser") {
   val inline = Gen.alphaNumStr
   property("Any alpha sequence followed by 'cr or crlf' is a paragraph of that sequence") = forAll(inline) { i =>
     (i != null && i != "") ==> {
