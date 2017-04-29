1c79316d5437ca3ba5b66f68ef3d1cf29f33e509
DRouh <dimerouh@gmail.com>
2017-04-29 09:11:00 +0300

    List parser code cleanup

--- src/main/scala/com/mdpeg/ListBlockParser.scala
+++ src/main/scala/com/mdpeg/ListBlockParser.scala
@@ -5,29 +5,22 @@ import org.parboiled2.{CharPredicate, Rule0, Rule1}
 trait ListBlockParser extends PrimitiveRules {
   import CharPredicate._
 
-  def list : Rule1[Block] = rule { unorderedList }//| orderedList }
-  def unorderedList : Rule1[UnorderedList] = ???
-  def orderedList : Rule1[OrderedList] = ???
+  def list: Rule1[Block] = rule { unorderedList }
+  def unorderedList: Rule1[UnorderedList] = ???
+  def orderedList: Rule1[OrderedList] = ???
 
   // aux rules
+  def bullet: Rule0     = rule { nonIndentSpace ~ anyOf("+-*") ~ sp.+ }
+  def enumerator: Rule0 = rule { nonIndentSpace ~ Digit.+ ~ "." ~ sp.+ }
 
-  def bullet : Rule0 = rule { nonIndentSpace ~ anyOf("+-*") ~ sp.+ }
-  def enumerator : Rule0 = rule { nonIndentSpace ~ Digit.+ ~ "." ~ sp.+}
-
-  def bulletListTight = rule { (capture(bulletListItem.+) ~> ((x:Any,y:Any) => toRawMd(x, y))) ~ blankLine.* ~ !bulletListSparse }
-  def bulletListSparse = rule { capture((bulletListItem ~ blankLine.*).+) ~> ((x:Any,y:Any) => toRawMd(x, y)) }
-  //def bulletListItem = rule { !horizontalRule ~ bullet ~ listBlock ~ listContinuationBlock.* }
-  def bulletListItem = rule { capture(!horizontalRule ~ bullet ~ listBlock ~ listContinuationBlock.*) ~> ((x:Any) => x) }
-
-//  def listBlock : Rule1[(String, String)] = rule {
-//    capture(anyLine) ~ capture(zeroOrMore(!(indent.? ~ bulletListItem) ~ !blankLine ~ !(indent ~ bullet) ~ indentedLine.?)) ~> ((x:String, y: String) => g(x, y))
-//  }
-  def listBlock :Rule0 = rule { anyLine ~ zeroOrMore( !indent.? ~ !bulletListItem ~ !blankLine ~ !indent ~ !bullet ~ indentedLine.?) }
-  //def listContinuationBlock : Rule1[Vector[String]]  = rule { ( capture(blankLine.+) ~ capture((indent ~ listBlock).+)) ~> ((x:Any, y:Any, z:Any) => f(x)) }
+  def bulletListTight       = rule { (capture(bulletListItem.+) ~> ((x: Any, y: Any) => toRawMd(x, y))) ~ blankLine.* ~ !bulletListSparse}
+  def bulletListSparse      = rule { capture((bulletListItem ~ blankLine.*).+) ~> ((x: Any, y: Any) => toRawMd(x, y)) }
+  def bulletListItem        = rule { capture(!horizontalRule ~ bullet ~ listBlock ~ listContinuationBlock.*) ~> ((x: Any) => x) }
+  def listBlock: Rule0      = rule { anyLine ~ zeroOrMore(!indent.? ~ !bulletListItem ~ !blankLine ~ !indent ~ !bullet ~ indentedLine.?) }
   def listContinuationBlock = rule { blankLine.+ ~ (indent ~ listBlock).+ }
 
   //aux func
-  def toRawMd (x: Any, y:Any) = {
+  def toRawMd(x: Any, y: Any) = {
     println("x:" + x)
     println("y:" + y)
     Vector(Markdown(x.asInstanceOf[Vector[String]]))
