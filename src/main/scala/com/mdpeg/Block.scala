9df951db26fd41e3b16fcbe474f4b6355de7c1d1
DRouh <dimerouh@gmail.com>
2017-05-02 09:56:49 +0300

    Initial outline of the multiline tables parser and AST

--- src/main/scala/com/mdpeg/Block.scala
+++ src/main/scala/com/mdpeg/Block.scala
@@ -8,11 +8,19 @@ case object HorizontalRuleBlock extends Block
 final case class HeadingBlock(level: Int, inline: String) extends Block
 final case class Verbatim(inline: String) extends Block
 final case class BlockQuote(inline: String) extends Block
-final case class TableBlock(inline: String) extends Block
+
 
 // list cases
 final case class OrderedList(inline: Vector[Block]) extends Block
 final case class UnorderedList(inline: Vector[Block]) extends Block
+
+//
+final case class MultilineTableBlock(inline: String) extends Block
+
+sealed trait MultilineTableElement
+final case class MultilineTableTitle(inline: Markdown) extends MultilineTableElement
+final case class MultilineTableColumnHeader(inline: Markdown) extends MultilineTableElement
+final case class MultilineTableCell(inline: Markdown) extends MultilineTableElement
 
 /**
   * Raw markdown that is yet to be processed into blocks
---
+++ src/main/scala/com/mdpeg/MultilineTablesParser.scala
@@ -0,0 +1,8 @@
+package com.mdpeg
+
+import org.parboiled2._
+trait MultilineTablesParser extends PrimitiveRules {
+  this: Parser =>
+  import CharPredicate._
+
+}
