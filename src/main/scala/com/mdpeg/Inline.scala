a7e30791a09442dcd2b762c30b495a79a090396b
DRouh <dimerouh@gmail.com>
2017-05-09 17:56:46 +0300

    #10 Initial outline for italics

--- src/main/scala/com/mdpeg/Inline.scala
+++ src/main/scala/com/mdpeg/Inline.scala
@@ -2,3 +2,4 @@ package com.mdpeg
 
 sealed trait Inline
 case class Strong(inline: String) extends Inline //ToDo should be Inline instead of String
+case class Italics(inline: String) extends Inline //ToDo should be Inline instead of String
--- src/main/scala/com/mdpeg/InlineRules.scala
+++ src/main/scala/com/mdpeg/InlineRules.scala
@@ -7,25 +7,35 @@ trait InlineRules {
   import CharPredicate._
 
   def inline: Rule1[Inline] = rule(strong)
-  def strong: Rule1[Strong] = rule(strongStarred | strongUnderlined)
 
+  // strong
+  def strong: Rule1[Strong] = rule(strongStarred | strongUnderlined)
   def strongStarred: Rule1[Strong] = {
     def contents: Rule0 = rule((!(spnl ~ twoStar) ~ inlineChar).+)
 
     //ToDo instead of inlineChar there should be recursive call to inline rule
     rule(twoStar ~ !sp ~ !nl ~ capture(contents) ~ twoStar ~> (Strong(_)))
   }
-
   def strongUnderlined: Rule1[Strong] = {
     def contents: Rule0 = rule((!(spnl ~ twoUnder) ~ !twoUnder ~ inlineChar).+)
 
     //ToDo instead of inlineChar there should be recursive call to inline rule
     rule(twoUnder ~ !sp ~ !nl ~ capture(contents) ~ twoUnder ~ !AlphaNum ~> (Strong(_)))
   }
-
-
   def twoStar: Rule0 = rule("**" ~ !twoStar)
   def twoUnder: Rule0 = rule("__" ~ !twoUnder)
+
+  // italics
+  def italics: Rule1[Italics] = rule(italicsStarred|italicsUnderlined)
+  def italicsStarred: Rule1[Italics] = {
+    def contents= rule((strong | !(spnl ~ oneStar) ~ inlineChar).+)
+
+    ???
+  }
+
+  def italicsUnderlined: Rule1[Italics] = ???
+  def oneStar: Rule0 = rule("*" ~ !oneStar)
+  def oneUnder: Rule0 = rule("_" ~ !oneUnder)
 
   def label: Rule1[String] = rule("[" ~ capture((!"]" ~ inlineChar).+) ~ "]") // [label]
   def title: Rule1[String] = rule { // 'title' or "title" for reference
