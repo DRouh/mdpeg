de0d7473f8f92a5aa163ba57da68f86be9a17b58
DRouh <dimerouh@gmail.com>
2017-05-09 15:04:51 +0300

    #11 Initial outline of the strong/bold rule

--- src/main/scala/com/mdpeg/InlineRules.scala
+++ src/main/scala/com/mdpeg/InlineRules.scala
@@ -5,7 +5,16 @@ import org.parboiled2._
 trait InlineRules {
   this: Parser with PrimitiveRules =>
 
-  def label : Rule1[String] = rule("[" ~ capture((!"]" ~ inline).+) ~ "]") // [label]
+  def inline : Rule0 = rule(strong)
+  def strong : Rule0 = rule(strongStarred|strongUnderlined)
+  def strongStarred : Rule0  = ???
+  def strongUnderlined  : Rule0 = ???
+
+  def twoStar  :Rule0 = rule("**" ~ !twoStar)
+  def twoUnder :Rule0 = rule("__" ~ !twoUnder)
+
+
+  def label : Rule1[String] = rule("[" ~ capture((!"]" ~ inlineChar).+) ~ "]") // [label]
   def title : Rule1[String] = rule { // 'title' or "title" for reference
     "\"" ~ capture((!"\"" ~ !nl ~ anyChar).*) ~ "\"" |
     "'"  ~ capture((!"'" ~ !nl   ~ anyChar).*) ~ "'"
