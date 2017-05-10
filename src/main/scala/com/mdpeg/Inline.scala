ccbbd156aef75649d638d998dc20a4536cd184e2
DRouh <dimerouh@gmail.com>
2017-05-10 09:54:09 +0300

    #10 Add support for italics
    Add tests for strong italics and vice versa.
    Add tests checking that double italics and double strong are forbidden.

--- src/main/scala/com/mdpeg/Inline.scala
+++ src/main/scala/com/mdpeg/Inline.scala
@@ -2,7 +2,7 @@ package com.mdpeg
 
 sealed trait Inline
 case class Strong(inline: Seq[Inline]) extends Inline
-case class Italics(inline: String) extends Inline //ToDo should be Inline instead of String
+case class Italics(inline: Seq[Inline]) extends Inline
 case class Text(inline: String) extends Inline
 case object Space extends Inline
 case object LineBreak extends Inline
