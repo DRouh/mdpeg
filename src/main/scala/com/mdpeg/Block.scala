57076288f34096ad0c8c61e39a304b0f5637656b
DRouh <dimerouh@gmail.com>
2017-05-13 13:43:55 +0300

    Change block classes order

--- src/main/scala/com/mdpeg/Block.scala
+++ src/main/scala/com/mdpeg/Block.scala
@@ -4,29 +4,20 @@ sealed trait Block
 
 final case class Plain(inline: InlineContent) extends Block
 final case class Paragraph(inline: InlineContent) extends Block
-case object HorizontalRuleBlock extends Block
 final case class HeadingBlock(level: Int, inline: InlineContent) extends Block
-final case class Verbatim(inline: String) extends Block
 final case class BlockQuote(inline: String) extends Block
-final case class ReferenceBlock(inline: InlineContent, target: Target) extends Block
-
-// list cases
-final case class OrderedList(inline: Vector[Block]) extends Block
 final case class UnorderedList(inline: Vector[Block]) extends Block
-
-// multiline table
+final case class OrderedList(inline: Vector[Block]) extends Block
+final case class Verbatim(inline: String) extends Block
+final case class ReferenceBlock(inline: InlineContent, target: Target) extends Block
+final case class Markdown(inline: String) extends Block
+final case object HorizontalRuleBlock extends Block
 final case class MultilineTableBlock(relativeWidth: Vector[Float],
+                                     // ToDo capture alignment
                                      caption: Option[MultilineTableCaption],
                                      head: Option[MultilineTableRow],
                                      body: Vector[MultilineTableColumn]) extends Block
+
 sealed trait MultilineTableElement
 case class MultilineTableCaption(inline: Markdown) extends MultilineTableElement
 case class MultilineTableCell(inline: Markdown) extends MultilineTableElement
-// ToDo capture alignment
-
-/**
-  * Raw markdown that is yet to be processed into blocks
-  *
-  * @param inline - raw string
-  */
-final case class Markdown(inline: String) extends Block
