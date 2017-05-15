975214eb7ed4c8db2837b92751fc51dc6819f136
DRouh <dimerouh@gmail.com>
2017-05-15 19:24:36 +0300

    #27 Initial outline of transform Tree function

--- src/main/scala/com/mdpeg/ASTTransform.scala
+++ src/main/scala/com/mdpeg/ASTTransform.scala
@@ -1,8 +1,33 @@
 package com.mdpeg
 
-import org.parboiled2.ParseError
+import org.parboiled2.{ErrorFormatter, ParseError}
+import scala.util.{Failure, Success}
 
 object ASTTransform {
+
+  def processMarkdown(m: Markdown): Either[String, Seq[Block]] = {
+    val Markdown(inline) = m
+    val parser = new BlockParser(inline)
+    parser.InputLine.run() match {
+      case Success(node) => Right(node)
+      case Failure(e: ParseError) =>
+        val error = parser.formatError(e, new ErrorFormatter(showTraces = true))
+        Left(error)
+      case Failure(e) => sys.error(e.getMessage)
+    }
+  }
+
+  def transformNode(b: Block): Either[String, Seq[Block]] = {
+    b match {
+      case m @ Markdown(_) =>
+        processMarkdown(m) match {
+          case l @ Left(_) => l
+          case r @ Right(_) => r
+        }
+      case _  => Right(Vector(b))
+    }
+  }
+
   def transformTree(tree: Seq[Block]): Either[ParseError, Seq[Block]] = {
     ???
   }
---
+++ src/test/scala/ASTTransformSpec.scala
@@ -0,0 +1,21 @@
+import com.mdpeg.ASTTransform._
+import com.mdpeg._
+import org.scalatest.{FlatSpec, Matchers}
+
+class ASTTransformSpec extends FlatSpec with Matchers {
+
+  it should "Should extract one level of Markdown to block seq" in {
+    val term =
+      s"""> ${TestData.blockQuoteLineOne}
+         |> ${TestData.blockQuoteLineTwo}
+         |> ${TestData.blockQuoteLineThree}
+         |""".stripMargin
+    val parsed = new BlockParser(term).blockQuote.run().get
+
+    parsed.inline.map(transformNode).map(_.getOrElse(Vector.empty[Block])) shouldEqual
+    Vector(
+      Vector(Plain(Vector(Text("This"), Space, Text("is"), Space, Text("quote")))),
+      Vector(Plain(Vector(Text("and"), Space, Text("should"), Space, Text("span"), Space, Text("several")))),
+      Vector(Plain(Vector(Text("yet"), Space, Text("another"), Space, Text("line"), Space, Text("for"), Space, Text("the"), Space, Text("block")))))
+  }
+}
