497573f23e9b17bca0e62b402621962bb0893664
DRouh <dimerouh@gmail.com>
2017-06-06 09:22:02 +0300

    #27 Re-implement test for one-level Markdown parsing

--- src/test/scala/ASTTransformSpec.scala
+++ src/test/scala/ASTTransformSpec.scala
@@ -4,25 +4,16 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ASTTransformSpec extends FlatSpec with Matchers {
 
-  it should "Should extract one level of Markdown to block seq" in {
-    val term =
-      s"""> ${TestData.blockQuoteLineOne}
-         |> ${TestData.blockQuoteLineTwo}
-         |> ${TestData.blockQuoteLineThree}
-         |""".stripMargin
-    val parsed = new BlockParser(term).blockQuote.run().get
+  it should "transform one level of Markdown to block seq" in {
+    val rawAstTree = Vector(
+      Markdown("This is quote"),
+      Markdown("and should span several"),
+      Markdown("yet another line for the block")
+    )
 
-//    parsed.inline.map(transformNode).map(_.getOrElse(Vector.empty[Block])) shouldEqual
-//    Vector(
-//      Vector(
-//        Plain(Vector(Text("This"), Space, Text("is"), Space, Text("quote")))),
-//      Vector(
-//        Plain(Vector(Text("and"), Space, Text("should"), Space, Text("span"), Space, Text("several")))),
-//      Vector(
-//        Plain(Vector(Text("yet"), Space, Text("another"), Space, Text("line"), Space, Text("for"), Space, Text("the"), Space, Text("block"))))
-//    )
+    val transformedTree = rawAstTree |> transformTree
 
-    parsed.inline |> transformTree  shouldEqual
+    transformedTree shouldEqual
     Right(
       Vector(
         Vector(Plain(Vector(Text("This"), Space, Text("is"), Space, Text("quote")))),
