7e395b6a5eac6007e2c7499cf4598ac56ec03b02
DRouh <dimerouh@gmail.com>
2017-06-08 21:53:55 +0300

    #27 Simplify Multiline table tree transformation

--- src/main/scala/com/mdpeg/ASTTransform.scala
+++ src/main/scala/com/mdpeg/ASTTransform.scala
@@ -2,12 +2,10 @@ package com.mdpeg
 
 import org.parboiled2.{ErrorFormatter, ParseError}
 
-import scala.collection.immutable
 import scala.util.{Failure, Success}
 
 object ASTTransform {
   type FailureMessage = String
-  type MacroBlock = Seq[Block]
 
   def processMarkdown(m: Markdown): Either[FailureMessage, Seq[Block]] = {
     val Markdown(inline) = m
@@ -47,50 +45,55 @@ object ASTTransform {
       }
     }
 
+    def transformTable(mt: MultilineTableBlock) = {
+      val MultilineTableBlock(relativeWidth, caption, head, body) = mt
+      val transformedCaption = caption map { c =>
+        val MultilineTableCaption(md) = c
+        processMarkdownContainer(md)(blocks => Right(Vector(MultilineTableCaption(blocks.toVector))))
+      }
+
+      val transformedHead = head map { head =>
+        processMarkdownContainer {
+          head.flatMap { h =>
+            val MultilineTableCell(bs) = h
+            bs
+          }
+        }(blocks => Right(Vector(MultilineTableCell(blocks.toVector))))
+      }
+
+      val transformedBodyColumns = body.
+        map { body =>
+          body.
+            map { h =>
+              val MultilineTableCell(blocks) = h
+              blocks
+            }.
+            map(processMarkdownContainer(_)(blocks => Right(Vector(MultilineTableCell(blocks.toVector))))) |>
+            join |>
+            (_.map(x => x.map(c => MultilineTableCell(c.toVector)).toVector))
+        } |>
+        join |>
+        (_.map(_.map(_.toVector).toVector))
+
+      (transformedCaption, transformedHead, transformedBodyColumns) match {
+        case (Some(Left(l)), _, _) => Left(l)
+        case (_, Some(Left(l)), _) => Left(l)
+        case (_, _, Left(l)) => Left(l)
+
+        case (c, h, b) =>
+          val cc = c.map(x => MultilineTableCaption(x.right.get.toVector))
+          val hh = h.map(y => Vector(MultilineTableCell(y.right.get.toVector)))
+          val bb = b.right.get
+          Right(Vector(MultilineTableBlock(relativeWidth, cc, hh, bb)))
+      }
+    }
+
     block match {
       case m @ Markdown(_) => processMarkdownBlock(m)
       case UnorderedList(v) => processMarkdownContainer(v)(blocks => Right(Vector(UnorderedList(blocks.toVector))))
       case OrderedList(v) => processMarkdownContainer(v)(blocks => Right(Vector(OrderedList(blocks.toVector))))
       case BlockQuote(v) => processMarkdownContainer(v)(blocks => Right(Vector(BlockQuote(blocks.toVector))))
-      case MultilineTableBlock(relativeWidth, caption, head, body) =>
-        val transformedCaption: Option[Either[Seq[FailureMessage], Seq[Block]]] = caption map { c =>
-          val MultilineTableCaption(md) = c
-          processMarkdownContainer(md)(blocks => Right(Vector(MultilineTableCaption(blocks.toVector))))
-        }
-
-        val transformedHead: Option[Either[Seq[FailureMessage], Seq[Block]]] = head map { head =>
-          val cells = head.flatMap { h =>
-            val MultilineTableCell(z) = h
-            z
-          } // ToDo Validate that flatten is correct here
-          processMarkdownContainer(cells)(blocks => Right(Vector(MultilineTableCell(blocks.toVector))))
-        }
-
-        val transformedBody: Vector[Either[Seq[FailureMessage], Vector[MultilineTableCell]]] = body map { body =>
-          body.map { h =>
-              val MultilineTableCell(blocks) = h
-              blocks
-            }
-            .map(processMarkdownContainer(_)(blocks => Right(Vector(MultilineTableCell(blocks.toVector))))) |>
-            join |>
-            (_.map(x => x.map(c => MultilineTableCell(c.toVector)).toVector))
-        }
-
-        // ToDo this is unsafe and should be re-implemented
-        val transformedBodyColumns: Either[Seq[FailureMessage], Vector[MultilineTableColumn]] =
-          transformedBody |> join |> (_.map(_.map(_.toVector).toVector))
-
-        (transformedCaption, transformedHead, transformedBodyColumns) match {
-          case (Some(Left(l)), _, _) =>  Left(l)
-          case (_, Some(Left(l)), _) => Left(l)
-          case (_, _,  Left(l)) =>  Left(l)
-
-          case (c,h,b) =>
-            val cc = c.map(x => MultilineTableCaption(x.right.get.toVector))
-            val hh= h.map(y => Vector(MultilineTableCell(y.right.get.toVector)))
-            val bb = b.right.get
-            Right(Vector(MultilineTableBlock(relativeWidth, cc, hh, bb)))
-        }
+      case mt @ MultilineTableBlock(_, _, _, _) => transformTable(mt)
       case _ => Right(Vector(block))
     }
   }
@@ -104,7 +107,7 @@ object ASTTransform {
     }
   }
 
-  def transformTree(tree: Seq[Block]): Either[Seq[FailureMessage], Seq[MacroBlock]] = {
+  def transformTree(tree: Seq[Block]): Either[Seq[FailureMessage], Seq[Seq[Block]]] = {
     tree.map(transformNode) |> join
   }
 }
