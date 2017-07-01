2292ad1c6fb7619371c7db12dadb62d0d644a620
DRouh <dimerouh@gmail.com>
2017-07-01 14:31:28 +0300

    #39 Provide example code
    Provide a simple usage example

--- examples/src/main/scala/org/mdpeg/examples/ToHtmlExample.scala
+++ examples/src/main/scala/org/mdpeg/examples/ToHtmlExample.scala
@@ -1,7 +1,23 @@
 package org.mdpeg.examples
+import java.io.PrintWriter
 
-import org.mdpeg._
+import org.mdpeg.MarkdownParser
 
 object ToHtmlExample extends App {
+  val inputFilePath = "insert you file path here"      // for example, input.md
+  val outputHtmlFilePath = "insert you file path here" // for example, output.html
+  val fileText = scala.io.Source.fromFile(inputFilePath).mkString
 
+  //run markdown parser to get an AST
+  MarkdownParser.parse(fileText) match {
+    case Left(errors) => println(errors) // in this case something went wrong, meaning that parser failed to parse given document
+    case Right(ast) =>
+      //transform the AST to an HTML string
+      MarkdownParser.transformToHtml(ast) match {
+        case Left(error) => println(error) // error case
+        case Right(html) =>
+          //write the HTML to the output file
+          new PrintWriter(outputHtmlFilePath) { write(html); close }
+      }
+  }
 }
